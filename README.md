fcdd57e76ec734ac4dea3b9da8aa20f65ce95b24
keijun-kumagai <keijun.kumagai@dena.com>
2025-03-09 23:46:10 +0900

    feat: create sample golang server

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # playground-golang
-simple http server
+
+## Run
+
+```bash
+go run main.go
+```
+
+```bash
+curl localhost:8080/200
+curl localhost:8080/400
+curl localhost:8080/500
+```
+
+## Build
+
+```bash
+docker build -t docker-playground:latest .
+```
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+	})
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+	e.GET("/200", func(c echo.Context) error {
+		// return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+	e.GET("/400", func(c echo.Context) error {
+		// return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.JSON(http.StatusBadRequest, struct{ Status string }{Status: "BadRequest"})
+	})
+	e.GET("/500", func(c echo.Context) error {
+		// return c.HTML(http.StatusOK, "Hello, Docker! <3")
+		return c.JSON(http.StatusInternalServerError, struct{ Status string }{Status: "InternalServerError"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
+
+// Simple implementation of an integer minimum
+// Adapted from: https://gobyexample.com/testing-and-benchmarking
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
